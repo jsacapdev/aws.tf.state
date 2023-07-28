eeda1d832fccb88e2210c69ab8482f47d2508198
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-07-28 17:12:07 +0100

    d8a4d661-156e-42e4-a12c-f7dc7488fdfe

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,3 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket_name
+
+  force_destroy = true
 }
--- terraform.tf
+++ terraform.tf
@@ -3,9 +3,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.9.0"
+      version = "~> 5.9.0"
     }
   }
 
-  required_version = "1.5.3"
+  required_version = "~> 1.5.3"
 }
